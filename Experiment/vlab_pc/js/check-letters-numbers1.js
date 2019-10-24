9ee145da77f1370728934df69f8e9c3208c67f64
communication17 <56721242+communication17@users.noreply.github.com>
2019-10-24 23:24:54 +0530

    Add files via upload

---
+++ Experiment/vlab_pc/js/check-letters-numbers1.js
@@ -0,0 +1,75 @@
+function alphanumeric5(inputtxt)
+{ 
+var letters = /^[sheep]+$/;
+if(inputtxt.value.match(letters))
+{
+alert('Your Input is correct : you can try another');
+document.form2.text6.focus();
+return true;
+}
+else
+{
+alert('Your Input is not valid please try another');
+return false;
+}
+}
+function alphanumeric6(inputtxt)
+{ 
+var letters = /^[cot]+$/;
+if(inputtxt.value.match(letters))
+{
+alert('Your Input is correct : you can try another');
+document.form2.text7.focus();
+return true;
+}
+else
+{
+alert('Your Input is not valid please try another');
+return false;
+}
+}
+function alphanumeric7(inputtxt)
+{ 
+var letters = /^[curl]+$/;
+if(inputtxt.value.match(letters))
+{
+alert('Your Input is correct : you can try another');
+document.form2.text8.focus();
+return true;
+}
+else
+{
+alert('Your Input is not valid please try another');
+return false;
+}
+}
+function alphanumeric8(inputtxt)
+{ 
+var letters = /^[apple]+$/;
+if(inputtxt.value.match(letters))
+{
+alert('Your Input is correct : you can try another');
+document.form2.text9.focus();
+return true;
+}
+else
+{
+alert('Your Input is not valid please try another');
+return false;
+}
+}
+function alphanumeric9(inputtxt)
+{ 
+var letters = /^[air]+$/;
+if(inputtxt.value.match(letters))
+{
+alert('Your Input is correct : you can try another');
+document.form2.text10.focus();
+return true;
+}
+else
+{
+alert('Your Input is not valid please try another');
+return false;
+}
+}
